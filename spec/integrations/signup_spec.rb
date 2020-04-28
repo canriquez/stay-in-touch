f89b56195991b94c23e6eef8275766f9b8ea3dc4
HADDADSOHAIB <souhaib.haddad@gmail.com>
2020-04-28 22:32:05 +0000

    add signup integration text

---
+++ spec/integrations/signup_spec.rb
@@ -0,0 +1,17 @@
+ 
+require 'rails_helper'
+
+RSpec.describe 'User creates a new registered user', type: :feature do
+  scenario 'they access the home page and click the signup button' do
+    visit new_user_registration_path
+
+    fill_in 'user[name]', with: 'exampleUser'
+    fill_in 'user[email]', with: 'example@example.com'
+    fill_in 'user[password]', with: 'password'
+    fill_in 'user[password_confirmation]', with: 'password'
+
+    click_button 'Sign up'
+
+    expect(page).to have_content('exampleUser')
+  end
+end
