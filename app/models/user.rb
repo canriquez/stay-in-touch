2d01148ed14a9324532c21af5d9a922cb9e5d949
Carlos Anriquez <canriquez@gmail.com>
2020-04-28 14:16:06 -0300

    adding friendship and key methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,39 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+
+  def my_friends
+    friends = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends += inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends.compact   #cleanup
+  end
+
+  #My sent friendship requests not accepted or denied 
+  def pending_requests
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed && !friendship.denied}.compact
+  end
+
+  #recieved friendship requests that I have not accepted or denied 
+  def recieved_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed && !friendship.denied}.compact
+  end
+
+  def accept_request(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def reject_request(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.denied = true
+    friendship.save
+  end
+
+  def is_friend?(user)
+    friends.include?(user)
+  end
+
 end
